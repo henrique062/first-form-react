c76f6705ff684d4232ce879f533c80bfd5346d38
Henrique <visioneplays@gmail.com>
2023-03-03 09:49:26 -0300

    feat(Adição-banner-rodape):
    Adicionado arquivo SVG do banner externo à pasta de assets do projeto;
    Criado um novo componente React para renderizar o banner;
    Implementado o componente do banner na página principal da aplicação;
    Estilizado o banner para se adequar à aparência da aplicação.

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,9 +1,22 @@
-export default function inicio (){
+import Banner from "componetes/Banner";
+import styles from './Inicio.module.css';
+import posts from 'json/posts.json';
+import Post from "componetes/Post";
+
+
+export default function Inicio (){
 
     return(
+        <main>
+            <Banner />
 
-        <h1>Olá Mundo!</h1>
-
+            < ul className={styles.posts}>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Post post={post} />
+                    </li>
+                ))}
+            </ul>
+        </main>
     )
-
 }
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import Rodape from "componetes/Rodape";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./componetes/Menu";
 import Inicio from "./paginas/Inicio";
@@ -15,8 +16,9 @@ function AppRoutes() {
       <Route path="/sobremim" element={<SobreMim />} />
       <Route path="*" element={<div>Página não encontrada</div>} />
     </Routes>
-  </BrowserRouter>
 
+    <Rodape />
+  </BrowserRouter>
   );
 }
 
